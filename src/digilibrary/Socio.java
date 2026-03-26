bc998e1505988ec92b65de5ccc32a556a3c46429
Ruben <ruben.carreira06@somo.eus>
2026-03-26 17:07:27 +0100

    getters y setter de la clase socios

--- src/digilibrary/Socio.java
+++ src/digilibrary/Socio.java
@@ -8,4 +8,21 @@ public class Socio {
         this.nombre = nombre;
         this.DNI = dni;
     }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+    
 }
