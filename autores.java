c680eaf45a408fc550747960c436e8cc3369c1bd
Markel ZZ <markelvm1@gmail.com>
2026-03-26 20:35:50 +0100

    Añadida clase Autores

---
+++ autores.java
@@ -0,0 +1,20 @@
+public class Autor {
+
+    private int cod_autor;
+
+    public Autor(int cod_autor) {
+        this.cod_autor = cod_autor;
+    }
+
+    public int getCodAutor() {
+        return cod_autor;
+    }
+
+    public void setCodAutor(int cod_autor) {
+        this.cod_autor = cod_autor;
+    }
+
+    public void agregarLibro(Libro l) {
+        System.out.println("Libro agregado: " + l);
+    }
+}
