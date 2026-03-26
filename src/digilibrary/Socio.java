36e63ba3d1e9c8535f78e4e6df618c9922da535d
Ruben <ruben.carreira06@somo.eus>
2026-03-26 17:06:18 +0100

    creacion de la clase socios

---
+++ src/digilibrary/Socio.java
@@ -0,0 +1,11 @@
+package digilibrary;
+
+public class Socio {
+	private String nombre;
+    private String DNI;
+
+    public Socio(String nombre, String dni) {
+        this.nombre = nombre;
+        this.DNI = dni;
+    }
+}
