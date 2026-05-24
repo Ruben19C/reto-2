a658de1a445c82b71d69d5e5be10f9260c40daef
Ruben <ruben.carreira06@somo.eus>
2026-05-24 17:28:47 +0200

    cambio del url de la base de datos

--- src/digilibrary/ConexionBD.java
+++ src/digilibrary/ConexionBD.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConexionBD {
-	private static final String URL = "jdbc:mysql://localhost:3306/biblioteca_db";
+	private static final String URL = "jdbc:mysql://localhost:3306/digilibrary";
     private static final String USER = "root";
     private static final String PASSWORD = ""; // Déjalo vacío si usan XAMPP
 
